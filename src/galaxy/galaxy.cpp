5319d9c80f1b6c56b5dc5f1b0640b58d956c2597
Sam Kellett <sam@wavestore.com>
2014-01-17 17:05:29 +0000

    Turned off vsync

--- src/galaxy/galaxy.cpp
+++ src/galaxy/galaxy.cpp
@@ -35,7 +35,7 @@ int Galaxy::exec()
     glfwTerminate();
     return 1;
   }
-  
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -54,6 +54,7 @@ int Galaxy::exec()
       component->render(dt);
     }
     
+    glfwSwapInterval(0);
     glfwSwapBuffers(window);
     glfwPollEvents();
 
