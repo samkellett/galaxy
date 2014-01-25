193911529b5b2edb86a283ba71f15a3a046f929d
Sam Kellett <samkellett@gmail.com>
2014-01-25 17:38:12 +0000

    Added base class for Gui widgets

---
+++ src/galaxy/widgets/widget.h
@@ -0,0 +1,15 @@
+#ifndef GALAXY_WIDGET_H
+#define GALAXY_WIDGET_H
+
+namespace gxy {
+namespace widget {
+
+class Widget
+{
+  virtual void render() = 0;
+};
+
+} // namespace widget
+} // namespace gxy
+
+#endif // GALAXY_WIDGET_H
