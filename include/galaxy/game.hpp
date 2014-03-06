8bff1fd96d4b46753428bd34a2858ef5a9022bed
Sam Kellett <samkellett@gmail.com>
2014-03-06 06:54:21 +0000

    Replaced the need for a registerable mixin with a lambda

--- include/galaxy/game.hpp
+++ include/galaxy/game.hpp
@@ -1,18 +1,20 @@
 #include <type_traits>
 
-#include "mixins/registerable.h"
+#include <yaml-cpp/yaml.h>
 
 namespace gxy {
 
 template <typename T>
 void Game::registerComponent(const std::string &id)
 {
-  static_assert(std::is_base_of<mixins::Registerable<T>, T>::value, "Cannot register a non-Registerable component.");
+  static_assert(std::is_base_of<Component, T>::value, "Cannot register a class that isn't a Component as a component!");
   if (loaders_.count(id)) {
     throw component_id_exists(id);
   }
 
-  loaders_.insert({id, &T::registerComponent});
+  loaders_.insert({id, [] (const YAML::Node &node) -> std::shared_ptr<Component> {
+    return std::make_shared<T>(node.as<T>());
+  }});
 }
 
 } // namespace gxy
