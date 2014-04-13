28de73735335f0e7778134288fe7fbd7d10a694b
Sam Kellett <samkellett@gmail.com>
2014-04-13 12:28:19 +0100

    Added more gfx opengl methods

--- include/galaxy/gfx.h
+++ include/galaxy/gfx.h
@@ -14,6 +14,10 @@ namespace gfx {
 // A:
 inline void attachShader(GLuint program, GLuint shader);
 // B:
+inline void bindBuffer(GLenum target, GLuint buffer);
+inline void bindTexture(GLenum target, GLuint texture);
+inline void blendFunc(GLenum sfactor, GLenum dfactor);
+inline void bufferData(GLenum target, GLsizeiptr size, const GLvoid *data, GLenum usage);
 // C:
 inline void clear(GLbitfield mask);
 inline void clearColor(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
@@ -21,10 +25,21 @@ inline void compileShader(GLuint shader);
 inline GLuint createProgram();
 inline GLuint createShader(GLenum type);
 // D:
+inline void deleteBuffers(GLsizei n, const GLuint *buffers);
+inline void deleteSamplers(GLsizei n, const GLuint *samplers);
+inline void deleteTextures(GLsizei n, const GLuint *textures);
+inline void disable(GLenum cap);
+inline void drawArrays(GLenum mode, GLint first, GLsizei count);
 // E:
+inline void enable(GLenum cap);
 // F:
 // G:
+inline void genBuffers(GLsizei n, GLuint *buffers);
+inline void generateMipmap(GLenum target);
+inline void genSamplers(GLsizei n, GLuint *samplers);
+inline void genTextures(GLsizei n, GLuint *textures);
 inline const GLubyte *getString(GLenum name);
+inline GLint getUniformLocation(GLuint program, const GLchar *name);
 // H:
 // I:
 // J:
@@ -32,15 +47,22 @@ inline const GLubyte *getString(GLenum name);
 // L:
 inline void linkProgram(GLuint program);
 // M:
+inline void *mapBuffer(GLenum target, GLenum access);
+inline void *mapBufferRange(GLenum target, GLintptr offset, GLsizeiptr length, GLbitfield access);
 // N:
 // O:
 // P:
 // Q:
 // R:
 // S:
+inline void sampleParamter(GLuint sampler, GLenum pname, GLint param);
 inline void shaderSource(GLuint shader, GLsizei count, const GLchar *const *string, const GLint *length);
 // T:
+inline void texImage2D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid * data);
 // U:
+template <typename T> inline void uniform(GLint location, const T &value);
+template <typename T> inline void uniform(GLint location, GLboolean transpose, const T &value);
+inline bool unmapBuffer(GLenum target);
 // V:
 // W:
 // X:
--- include/galaxy/gfx.hpp
+++ include/galaxy/gfx.hpp
@@ -11,7 +11,32 @@ void attachShader(GLuint program, GLuint shader)
   glAttachShader(program, shader);
   assert(glGetError() == GL_NO_ERROR);
 }
+
 // B:
+void bindBuffer(GLenum target, GLuint buffer)
+{
+  glBindBuffer(target, buffer);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void bindTexture(GLenum target, GLuint texture)
+{
+  glBindTexture(target, texture);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void blendFunc(GLenum sfactor, GLenum dfactor)
+{
+  glBlendFunc(sfactor, dfactor);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void bufferData(GLenum target, GLsizeiptr size, const GLvoid *data, GLenum usage)
+{
+  glBufferData(target, size, data, usage);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
 // C:
 void clear(GLbitfield mask)
 {
@@ -71,12 +96,80 @@ GLuint createShader(GLenum type)
 }
 
 // D:
+void deleteBuffers(GLsizei n, const GLuint *buffers)
+{
+  glDeleteBuffers(n, buffers);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void deleteSamplers(GLsizei n, const GLuint *samplers)
+{
+  glDeleteSamplers(n, samplers);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void deleteTextures(GLsizei n, const GLuint *textures)
+{
+  glDeleteTextures(n, textures);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void disable(GLenum cap)
+{
+  glDisable(cap);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void drawArrays(GLenum mode, GLint first, GLsizei count)
+{
+  glDrawArrays(mode, first, count);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
 // E:
+void enable(GLenum cap)
+{
+  glEnable(cap);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
 // F:
 // G:
+void genBuffers(GLsizei n, GLuint *buffers)
+{
+  glGenBuffers(n, buffers);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void generateMipmap(GLenum target)
+{
+  glGenerateMipmap(target);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void genSamplers(GLsizei n, GLuint *samplers)
+{
+  glGenSamplers(n, samplers);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+void genTextures(GLsizei n, GLuint *textures)
+{
+  glGenTextures(n, textures);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
 const GLubyte *getString(GLenum name)
 {
   auto ret = glGetString(name);
+  assert(glGetError() == GL_NO_ERROR);
+
+  return ret;
+}
+
+GLint getUniformLocation(GLuint program, const GLchar *name)
+{
+  auto ret = glGetUniformLocation(program, name);
   assert(glGetError() == GL_NO_ERROR);
 
   return ret;
@@ -105,12 +198,34 @@ void linkProgram(GLuint program)
 }
 
 // M:
+void *mapBuffer(GLenum target, GLenum access)
+{
+  auto ret = glMapBuffer(target, access);
+  assert(glGetError() == GL_NO_ERROR);
+
+  return ret;
+}
+
+void *mapBufferRange(GLenum target, GLintptr offset, GLsizeiptr length, GLbitfield access)
+{
+  auto ret = glMapBufferRange(target, offset, length, access);
+  assert(glGetError() == GL_NO_ERROR);
+
+  return ret;
+}
+
 // N:
 // O:
 // P:
 // Q:
 // R:
 // S:
+void sampleParameter(GLuint sampler, GLenum pname, GLint param)
+{
+  glSampleParameteri(sampler, pname, param);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
 void shaderSource(GLuint shader, GLsizei count, const GLchar *const *string, const GLint *length)
 {
   glShaderSource(shader, count, string, length);
@@ -118,7 +233,77 @@ void shaderSource(GLuint shader, GLsizei count, const GLchar *const *string, con
 }
 
 // T:
+void texImage2D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid * data)
+{
+  glTexImage2D(target, level, internalFormat, width, height, border, format, type, data);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
 // U:
+template <GLfloat>
+void uniform(GLint location, const GLfloat &value)
+{
+  glUniform1f(location, value);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+template <GLint>
+void uniform(GLint location, const GLint &value)
+{
+  glUniform1i(location, value);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+template <GLuint>
+void uniform(GLint location, const GLuint &value)
+{
+  glUniform1ui(location, value);
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+template <glm::vec2>
+void uniform(GLint location, const glm::vec2 &value)
+{
+  glUniform2fv(location, glm::value_ptr(value));
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+template <glm::vec3>
+void uniform(GLint location, const glm::vec3 &value)
+{
+  glUniform3fv(location, glm::value_ptr(value));
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+template <glm::vec4>
+void uniform(GLint location, const glm::vec4 &value)
+{
+  glUniform4fv(location, glm::value_ptr(value));
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+template <glm::mat3>
+void uniform(GLint location, GLboolean transpose, const glm::mat3 &value)
+{
+  glUniformMatrix3fv(location, transpose, glm::value_ptr(value));
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+template <glm::mat4>
+void uniform(GLint location, GLboolean transpose, const glm::mat4 &value)
+{
+  glUniformMatrix4fv(location, transpose, glm::value_ptr(value));
+  assert(glGetError() == GL_NO_ERROR);
+}
+
+bool unmapBuffer(GLenum target)
+{
+  auto ret = glUnmapBuffer(target);
+  assert(glGetError() == GL_NO_ERROR);
+
+  return ret;
+}
+
 // V:
 // W:
 // X:
