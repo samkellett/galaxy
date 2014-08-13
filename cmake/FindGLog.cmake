46853f8e2519bc0e4ed1b6c6271eb01901994b1b
Sam Kellett <samkellett@gmail.com>
2014-08-13 20:01:40 +0100

    Start again..

--- cmake/FindGLog.cmake
+++
@@ -1,8 +0,0 @@
-
-set(GLOG_INCLUDE_DIR)
-set(GLOG_LIBRARY)
-
-set(GLOG_ROOT_DIR "" CACHE PATH "Folder contains Google glog")
-
-find_path(GLOG_INCLUDE_DIR glog/logging.h PATHS ${GLOG_ROOT_DIR})
-find_library(GLOG_LIBRARY glog PATHS ${GLOG_ROOT_DIR} PATH_SUFFIXES lib lib64)
