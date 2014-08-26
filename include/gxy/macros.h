c29a3b48d4f7be8ed0912f33a96c1530ef3051da
Sam Kellett <samkellett@gmail.com>
2014-08-26 17:12:26 +0100

    Added two helpful macros

--- include/gxy/macros.h
+++ include/gxy/macros.h
@@ -1,3 +1,19 @@
 #pragma once
 
+#define GXY_PAIR(Pair, T1, first, T2, second) \
+  struct Pair \
+  { \
+    Pair() : first ( T1 ()), second ( T2 ()) {} \
+    Pair(const T1 &t1, const T2 &t2) : first (t1), second (t2) {} \
+  \
+    Pair &operator =(const Pair &other) \
+    { first = other.##first ; second = other.##second ; return *this; } \
+  \
+    T1 first ; \
+    T2 second ; \
+  }
+
 #define GXY_STR(str) #str
+
+#define GXY_UNUSED(var) (void) var
+
