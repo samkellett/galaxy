31c373aa765a89bf6db40bcb0b82259970765d4d
Sam Kellett <samkellett@gmail.com>
2014-08-28 20:59:47 +0100

    Added some initial methods to game class

--- include/gxy/game.h
+++ include/gxy/game.h
@@ -5,7 +5,18 @@ namespace gxy {
 class game
 {
 public:
-  void init();
+  game();
+
+  void tick();
+
+  void quit(const unsigned int error_code);
+  bool quitting() const;
+
+  unsigned int error_code() const;
+
+private:
+  unsigned int error_code_;
+  bool quitting_;
 };
 
 } // namespace gxy
--- src/gxy/game.cpp
+++ src/gxy/game.cpp
@@ -1,11 +1,34 @@
 #include "gxy/game.h"
 
 #include "gxy/gfx.h"
+#include "gxy/macros.h"
 
 namespace gxy {
 
-void game::init()
+game::game() :
+  error_code_(0),
+  quitting_(false)
 {
+}
+
+void game::tick()
+{
+}
+
+void game::quit(const unsigned int error_code)
+{
+  quitting_ = true;
+  error_code_ = error_code;
+}
+
+bool game::quitting() const
+{
+  return quitting_;
+}
+
+unsigned int game::error_code() const
+{
+  return error_code_;
 }
 
 } // namespace gxy
